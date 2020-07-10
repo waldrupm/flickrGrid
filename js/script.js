19db31a6e568e2eb32dc34b75e1393d9cfa1f190
Micheal Waldrup <waldrupm@gmail.com>
2020-07-10 14:36:45 -0400

    add image loading, add multiple tag search

--- js/script.js
+++ js/script.js
@@ -1,22 +1,69 @@
-const loadData = (page=1) => {
-  searchTags = $('#searchTags').val() || 'colorful';
-  tagsParam = searchTags;
+const loadData = (page=1, searchTags='flowers') => {
+  let photoRows = $('#photoRows');
+  photoRows.empty();
+  let tagsParam = searchTags;
   let pageParam = page === 1 ? '' : `&page=${page}`;
-  apiURL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=20e5bc680a66df4de1440248313a6a41&tags=${tagsParam}&format=json&nojsoncallback=1&per_page=24${pageParam}`;
+  apiURL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=20e5bc680a66df4de1440248313a6a41&tags=${tagsParam}&format=json&safe_search=1&nojsoncallback=1&per_page=24${pageParam}`;
+  // console.log('apiURL', apiURL)
   $.getJSON(apiURL, ( data ) => {
-      console.log('Got data');
-      console.log(data.photos.photo);
+      // console.log('Got data');
+      // console.log(data.photos.photo);
       images = data.photos.photo;
       images.forEach(image => {
         let { farm, server, id, secret, owner } = image;
         imgURL = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_w.jpg`;
-        console.log('imgURL', imgURL);
-        imgTitle = image.title.substr(0,50);
+        // console.log('imgURL', imgURL);
+        imgTitle = image.title.length > 17 ? (image.title.substr(0, 17) + "...") : image.title;
         imgLink = `https://www.flickr.com/photos/placeholder`; // TODO get image links
-
+        imgCardHTML = `
+        <div class="col-md-4">
+          <div class="card mb-4 text-white bg-dark">
+            <img class="card-img-top card-preview-img" src="${imgURL}" alt="Card image cap">
+            <div class="card-body">
+                <p class="card-text">${imgTitle}</p>
+               
+                <a href="${imgLink}" class="btn btn-outline-light btn-sm">Image Page</a>
+            </div>
+          </div>
+        </div> `; // End image card html
+        photoRows.append(imgCardHTML);
       });
       
   });
 };
+let currentTags;
+let currentPage;
+
+const changeTags = () => {
+  // Handle multiple tags for search
+  $('#introText').hide();
+  currentTags = $('#searchTags').val();
+  let tagsParam = '';
+  if (currentTags.includes(',')) {
+    // parse tags
+    let tags = currentTags.split(',');
+    console.log('tags', tags)
+    if (tags.length > 1) {
+      // format tags
+      tags = tags.map(tag => {
+        tag = tag.trim();
+        tag = encodeURIComponent(tag);
+        return tag;
+      });
+      tagsParam = tags.join(',');
+      // console.log('tagsParam', tagsParam)
+    }
+  } else {
+    tagsParam += currentTags;
+  }
+  $('#introText').text(`Showing photos for ${currentTags}`);
+  $('#introText').show();
+  loadData(page=1, searchTags=tagsParam);
+};
+$('#tagsForm').submit((event) => {
+  event.preventDefault();
+  changeTags();
+});
+$('#changeTagsButton').click(changeTags);
 
 loadData();
